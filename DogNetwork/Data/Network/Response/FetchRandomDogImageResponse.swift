942e15acc9bea41b64bece2020673eaf8f1b990c
talha heybeci <aheybeci@hotmail.com>
2024-07-21 17:27:00 +0300

    Network - Created network layer

---
+++ DogNetwork/Data/Network/Response/FetchRandomDogImageResponse.swift
@@ -0,0 +1,15 @@
+//
+//  FetchRandomDogImageResponse.swift
+//  DogNetwork
+//
+//  Created by talha heybeci on 21.07.2024.
+//
+
+import Foundation
+
+public struct FetchRandomDogImageResponse: Decodable {
+    public let message: String
+    public let status: String
+}
+
+
---
+++ DogNetwork/Model/Dog.swift
@@ -0,0 +1,14 @@
+//
+//  Dog.swift
+//  DogNetwork
+//
+//  Created by talha heybeci on 21.07.2024.
+//
+
+import Foundation
+
+public struct Dog: Decodable {
+    public let message: String
+    public let status: String
+}
+
