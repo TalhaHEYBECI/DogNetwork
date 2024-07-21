a7e03b7cf2182c8187e8ec7242703bdcc4648534
talha heybeci <aheybeci@hotmail.com>
2024-07-21 19:12:23 +0300

    Logger - recommitted

--- DogNetwork/Presentation/Interfaces/DogNetworkInterface.swift
+++ DogNetwork/Presentation/Interfaces/DogNetworkInterface.swift
@@ -20,13 +20,19 @@ public class DogNetworkInterface: ObservableObject {
     }
     
     public func fetchRandomDogImage() {
+        Logger.shared.log(message: "Fetching random dog image in Interface", level: .debug)
         fetchRandomDogImageUseCase.execute()
             .sink(receiveCompletion: { completion in
-                if case .failure(let error) = completion {
+                switch completion {
+                case .finished:
+                    Logger.shared.log(message: "Finished fetching random dog image in Interface", level: .info)
+                case .failure(let error):
                     self.errorMessage = error.localizedDescription
+                    Logger.shared.log(message: "Failed fetching random dog image in Interface with error: \(error)", level: .error)
                 }
             }, receiveValue: { dog in
                 self.dogImageURL = dog.message
+                Logger.shared.log(message: "Received dog image URL in Interface: \(dog.message)", level: .debug)
             })
             .store(in: &cancellables)
     }
--- DogNetwork/Utilities/Logger.swift
+++ DogNetwork/Utilities/Logger.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+public class Logger {
+    public static let shared = Logger()
+    private var isLoggingEnabled = true
+
+    private init() {}
+
+    public enum LogLevel: String {
+        case debug = "DEBUG"
+        case info = "INFO"
+        case warning = "WARNING"
+        case error = "ERROR"
+    }
+
+    public func enableLogging(_ enable: Bool) {
+        isLoggingEnabled = enable
+    }
+
+    public func log(message: String, level: LogLevel = .info, file: String = #file, function: String = #function, line: Int = #line) {
+        guard isLoggingEnabled else { return }
+        let timestamp = getCurrentTimestamp()
+        let fileName = (file as NSString).lastPathComponent
+        print("\(timestamp) [\(level.rawValue)] \(fileName):\(line) \(function) - \(message)")
+    }
+
+    private func getCurrentTimestamp() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        return dateFormatter.string(from: Date())
+    }
+}
