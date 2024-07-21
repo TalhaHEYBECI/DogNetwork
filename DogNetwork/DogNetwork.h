3e29f87d8bf8d992a2ec2f592cf7f36804d8e0a1
talha heybeci <aheybeci@hotmail.com>
2024-07-21 16:24:02 +0300

    Update .gitignore to ignore unnecessary files

---
+++ DogNetwork/DogNetwork.h
@@ -0,0 +1,16 @@
+//
+//  DogNetwork.h
+//  DogNetwork
+//
+//  Created by talha heybeci on 21.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DogNetwork.
+FOUNDATION_EXPORT double DogNetworkVersionNumber;
+
+//! Project version string for DogNetwork.
+FOUNDATION_EXPORT const unsigned char DogNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DogNetwork/PublicHeader.h>
